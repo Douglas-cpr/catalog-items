84f46671856871f9e4f6be8ab49c0f35dfee431f
Douglas <douglas-cprdev@hotmail.com>
2022-06-21 13:50:06 -0300

    config: add health checks to api and db

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using System.Net.Mime;
+using System.Text.Json;
 using Catalog.Repositories;
 using Catalog.Repositories.Contracts;
 using Catalog.Settings;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -10,11 +13,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 
 builder.Services.AddSingleton<IMongoClient>(serviceProvider => 
 {
-    var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    return new MongoClient(settings.ConnectionString);
+    return new MongoClient(mongoDbSettings.ConnectionString);
 });
 builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 builder.Services.AddControllers(options => {
@@ -22,6 +25,13 @@ builder.Services.AddControllers(options => {
 });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddHealthChecks()
+    .AddMongoDb(
+        mongoDbSettings.ConnectionString,
+        name: "mongodb", 
+        timeout: TimeSpan.FromSeconds(5),
+        tags: new[]{ "ready" });
 
 var app = builder.Build();
 
@@ -37,5 +47,30 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health/ready", new HealthCheckOptions {
+    Predicate = (check) => check.Tags.Contains("ready"),
+    ResponseWriter = async(context, report) => 
+    {
+        var result = JsonSerializer.Serialize(
+            new {
+                status = report.Status.ToString(),
+                checks = report.Entries.Select(entry => new {
+                    name = entry.Key,
+                    status = entry.Value.Status.ToString(),
+                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                    duration = entry.Value.Duration.ToString()
+                })
+            }
+        );
+
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        await context.Response.WriteAsync(result);
+    }
+});
+
+app.MapHealthChecks("/health/live", new HealthCheckOptions {
+    Predicate = (_) => false
+});
 
 app.Run();
