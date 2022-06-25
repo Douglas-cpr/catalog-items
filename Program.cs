0f63fe84c40774846217d68c10ed416c5382d3ed
Douglas <douglas-cprdev@hotmail.com>
2022-06-25 16:48:12 -0300

    config: add docker

--- Program.cs
+++ Program.cs
@@ -42,7 +42,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment()) 
+{
+    app.UseHttpsRedirection();
+}
+
 
 app.UseAuthorization();
 
